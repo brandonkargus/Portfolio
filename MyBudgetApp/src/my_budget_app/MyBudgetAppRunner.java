8810e4b99bcdf9629517ab93ec6305ee7d4cfe29
Brandon <brandonkargus@gmail.com>
2021-12-14 12:19:35 -0600

    added MyBudgetApp to portfolio

---
+++ MyBudgetApp/src/my_budget_app/MyBudgetAppRunner.java
@@ -0,0 +1,25 @@
+package my_budget_app;
+
+import java.util.Scanner;
+
+public class MyBudgetAppRunner {
+    Scanner scanner = new Scanner(System.in);
+
+    public static void runApp() {
+        System.out.println("Welcome to your Budgeting App!");
+        System.out.println();
+        System.out.println("Please select from the following menu options:");
+        System.out.println("1) View Account Balances");
+        System.out.println("2) View Overview Stats");
+        System.out.println("3) Customize Budget (add/update/delete income & expenses)");
+        System.out.println("4) Run Customized Reports");
+        System.out.println("5) Exit");
+
+    }
+
+    public static void main(String[] args) {
+        runApp();
+    }
+
+
+}
